a84ce84d50c3a6629295ccdb8c854fed6aae29ca
张纯 <chun.zhang@mini1.cn>
2021-06-29 11:38:21 +0800

    修改代理示例

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,8 @@
-/*
- * @Author: zhang_chun
- * @Date: 2020-11-17 15:45:01
- * @LastEditTime: 2021-03-29 14:11:59
- * @FilePath: \drawcanvas\src\setupProxy.js
- */
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(["/goods", "/sku", "/misc", "/m"], {
-      target: "", //配置你要代理请求的服务器地址
+      target: "http://118.89.54.96:7203/", //配置你要代理请求的服务器地址 这是示例
       changeOrigin: true,
     })
   );
